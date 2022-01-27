f375954b4d5906d35a2afebe4508fc1625ff398c
terrence <tmuthoka@gmail.com>
2022-01-26 21:44:29 -0500

    big numbers functions

---
+++ bignumbers.c
@@ -0,0 +1,33 @@
+
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdint.h>
+
+uint32_t
+br_i31_add(uint32_t *a, const uint32_t *b, uint32_t ctl)
+{
+        uint32_t cc;
+        size_t u, m;
+
+        cc = 0;
+        m = (a[0] + 63) >> 5;
+        for (u = 1; u < m; u ++) {
+                uint32_t aw, bw, naw;
+
+                aw = a[u];
+                bw = b[u];
+                naw = aw + bw + cc;
+                cc = naw >> 31;
+                a[u] = MUX(ctl, naw & (uint32_t)0x7FFFFFFF, aw);
+        }
+        return cc;
+}
+
+uint32_t *a; const uint32_t *b; uint32_t ct, res;
+
+*a = 344; *b = 344; ct = 344;
+res = br_i31_add(&a, &b, ctl);
+printf("res = %u\n", res);
+
